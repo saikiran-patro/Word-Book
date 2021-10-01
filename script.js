5673fa0985712ef137b87bcafcc7047100911d72
Sai Kiran Patro <saikiranpatro198@gmail.com>
2021-10-01 23:12:48 +0530

    final update

--- script.js
+++ script.js
@@ -5,53 +5,168 @@ let search=()=>{
 
 
      let string=document.getElementById("input_box").value;
-     let Word=" ";
-     let meaning=' ';
-     let example=' ';
-     let synonyms=' ';
-     let antonyms=' ';
+     string=string.replace(/\s+/g, ' ').trim()
+     let Word=0;
+     let meaning=0;
+     let example=0;
+     let synonyms=0;
+     let antonyms=0;
+
+    
+
+
      if(string!==''){
-
-        console.log(string)
-        let searchWord="https://api.dictionaryapi.dev/api/v2/entries/en/"+string;
-         fetch(searchWord).then(data=> data.json()).then( wordData =>{
-
-             Word=wordData[0].word;
-             
-             meaning=wordData[0].meanings[0].definitions[0].definition;
-             example=wordData[0].meanings[0].definitions[0].example;
-             synonyms=wordData[0].meanings[0].definitions[0].synonyms;
-             antonyms=wordData[0].meanings[0].definitions[0].antonyms;
-
-             
-
-             document.getElementById("word").innerText=Word;
-             document.getElementById("meaning").innerText=meaning;
-
-            
-                 
-             
-             document.getElementById("example").innerText=example;
-             document.getElementById("synonyms").innerText=synonyms;
         
-            if(synonyms.length!=0){
-
-                let string=""
-                for(let i=0;i<synonyms.length;i++){
-                    string=string+synonyms[i]+", "
-                }
-
-                document.getElementById("synonyms").innerText=string
-
-
-
-            }
-
-         })
 
          
-     }
+        let searchWord="https://api.dictionaryapi.dev/api/v2/entries/en/"+string;
 
+        try {
+            fetch(searchWord).then(data=> data.json()).then( wordData =>{
+
+                try{
+                Word=wordData[0].word;
+                if(example===0 && Word===0 && meaning===0 && synonyms===0 && antonyms===0){
+
+                    document.getElementById("word").innerText='None';
+                    document.getElementById("meaning").innerText='None';
+                    document.getElementById("example").innerText='None';
+                    document.getElementById("synonyms").innerText='None';
+                    document.getElementById("antonyms").innerText='None';
+                   
+                   
+            
+            
+            
+            
+            
+                }
+                
+                 }
+                 catch(error){
+
+                    alert("Please check your word")
+                 }
+                
+                meaning=wordData[0].meanings[0].definitions[0].definition;
+                example=wordData[0].meanings[0].definitions[0].example;
+   
+   
+                for(let i=0;i<wordData[0].meanings[0].definitions.length;i++){
+   
+                        if (wordData[0].meanings[0].definitions[i].synonyms.length!==0){
+                           synonyms=wordData[0].meanings[0].definitions[i].synonyms;
+                           break;
+               
+   
+                        }
+                        else{
+   
+                           synonyms=[]
+                        }
+   
+   
+                }
+                
+                for(let i=0;i<wordData[0].meanings[0].definitions.length;i++){
+   
+                   if (wordData[0].meanings[0].definitions[i].antonyms.length!==0){
+                      antonyms=wordData[0].meanings[0].definitions[i].antonyms;
+                      break;
+          
+   
+                   }
+                   else{
+   
+                      antonyms=[]
+                   }
+   
+   
+                  }
+                  for(let i=0;i<wordData[0].meanings[0].definitions.length;i++){
+   
+                   if (typeof(wordData[0].meanings[0].definitions[i].example)!="undefined"){
+                      example=wordData[0].meanings[0].definitions[i].example;
+                      break;
+          
+   
+                   }
+                   else{
+   
+                      example="None"
+                   }
+   
+   
+                  }
+           
+           
+   
+                
+   
+                document.getElementById("word").innerText=Word;
+                document.getElementById("meaning").innerText=meaning;
+   
+               
+                    
+                if (example){
+                   document.getElementById("example").innerText=example;
+   
+                }
+                
+                document.getElementById("synonyms").innerText=synonyms;
+           
+               if(synonyms.length!=0){
+   
+                   let string_=""
+                   for(let i=0;i<synonyms.length;i++){
+                       string_=string_+synonyms[i]+", "
+                   }
+   
+                   document.getElementById("synonyms").innerText=string_
+   
+   
+   
+               }
+               else{
+   
+   
+                   document.getElementById("synonyms").innerText="None"
+               }
+   
+               if(antonyms.length!=0){
+   
+   
+                   let string_=""
+                   for(let i=0;i<antonyms.length;i++){
+   
+                           string_=string_+antonyms[i]+", "
+   
+   
+                   }
+                   document.getElementById("antonyms").innerText=string_
+   
+               }
+               else{
+   
+                   document.getElementById("antonyms").innerText="None"
+               }
+   
+            })
+   
+            
+        }
+   
+            
+         catch (error) {
+            alert("Please search for a word")
+        }
+         
+    }
+    else{
+
+
+        alert("Please enter a word")
+    }
 
 
 
